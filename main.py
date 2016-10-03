3da1dbfb1faf067db081b8cf0f540cb115b117ab
crutchcorn <crutchcorn@gmail.com>
2016-10-03 05:09:04 -0700

    Major refactoring for #2
    This is incomplete. The last but of code needs to be further refactored. Other than that, we should be ready for #2 to strike us.

--- main.py
+++ main.py
@@ -3,18 +3,32 @@ from pkg_resources import iter_entry_points
 def getExtensions(group):
     extensions = {}
     handleExtPlugin = {}
+    entry_points = []
     for entry_point in iter_entry_points(group=group, name=None):
-        if entry_point.attrs[0] == "handleExt":
-            if extensions:
-                for extension in extensions:
-                    if extensions[extension] == entry_point.module_name: # if extensions["mp3"] == "plugins.lenovo "(this assumes this is second)
-                        extensions[extension] = entry_point.load()
-            handleExtPlugin[entry_point.module_name] = entry_point.load() # handleExtPlugin["plugins.lenovo"] (this assumes this is first)
-        elif entry_point.attrs[0] == "getExt":
-            if handleExtPlugin.get(entry_point.module_name, None): # handleExtPlugin["plugins.lenovo"] (this assumes this is second)
-                moduleLoad = handleExtPlugin[entry_point.module_name]
-            else:
-                moduleLoad = entry_point.module_name # This assumed this is first. It allows the extensions to become module_name to be handled by handleExt code
-            for extension in entry_point.load()(): # entry_point() would return list of extentions handleExt could handle
-                extensions[extension] = moduleLoad # extensions["mp3"]
+        entry_points.append(entry_point)
+
+    orderOfOps = [ 'handleExt', 'getExt'] # This will need to be read from a settings file and have defaults after #1 is implamented
+
+    # Actually sorts the entry_points
+    order = {key: i for i, key in enumerate(orderOfOps)}
+    entry_points.sort(key=lambda x: order[x.attrs[0]])
+
+    ## Sublists list by value
+    # IE: newlist[0] will return all entry_point items that match `handleExt`
+    entry_points = [[y for y in entry_points if y.attrs[0]==x] for x in order]
+
+    ### Now comes the exciting part of figuring out how I want to handle these types of things
+    ### The current code that resides is not good code and is not expandable in any way. This is just to get things working until I have more time.
+    ### Considering the following duct-tape that's falling apart. It's gotta be replaced ASAP
+    for epGroup in entry_points:
+        for entry_point in epGroup:
+            if entry_point.attrs[0] == "handleExt":
+                handleExtPlugin[entry_point.module_name] = entry_point.load() # handleExtPlugin["plugins.lenovo"] (this MUST be first)
+            elif entry_point.attrs[0] == "getExt":
+                if handleExtPlugin.get(entry_point.module_name, None): # handleExtPlugin["plugins.lenovo"] (this MUST be second)
+                    moduleLoad = handleExtPlugin[entry_point.module_name]
+                for extension in entry_point.load()(): # entry_point() would return list of extentions handleExt could handle
+                    extensions[extension] = moduleLoad # extensions["mp3"]
+    
+
     return extensions
