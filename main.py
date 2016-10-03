ade467702043290eb51c60802bc96b42bf8ad22c
crutchcorn <crutchcorn@gmail.com>
2016-10-03 10:51:23 -0700

    #2 now completely finished.
    All that's left now is #1

--- main.py
+++ main.py
@@ -18,17 +18,11 @@ def getExtensions(group):
         if y.attrs[0] == x:
             funcDict.setdefault(y.attrs[0], []).append(y)
 
-
-    ### Now comes the exciting part of figuring out how I want to handle these types of things
-    ### The current code that resides is not good code and is not expandable in any way. This is just to get things working until I have more time.
-    ### Considering the following duct-tape that's falling apart. It's gotta be replaced ASAP
     extensions = {}
-    handleExtPlugin = {}
-    for handleEx in funcDict['handleExt']:
-        handleExtPlugin[handleEx.module_name] = handleEx.load() # handleExtPlugin["plugins.lenovo"] (this MUST be first)
     for getEx in funcDict['getExt']:
-        moduleLoad = handleExtPlugin[getEx.module_name] # handleExtPlugin["plugins.lenovo"] (this MUST be second)
-        for extension in getEx.load()(): # entry_point() would return list of extentions handleExt could handle
-            extensions[extension] = moduleLoad # extensions["mp3"]
+        for handle in funcDict[getEx.load()(True)]:
+            if handle.module_name == getEx.module_name:
+                for extension in getEx.load()():
+                    extensions[extension] = handle.load()
 
     return extensions
